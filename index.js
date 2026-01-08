5d09cb841c16b096b210a30795a40659765d2e5a
Macash.Andrei <andrei.makash@yandex.ru>
2026-01-08 16:26:05 +0300

    изменен index.js

--- index.js
+++ index.js
@@ -258,6 +258,6 @@ app.post('/api/data/:orgId', async (req, res) => {
 });
 
 // Запуск сервера
-app.listen(PORT,'0.0.0.0', () => {
+app.listen(PORT,() => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
